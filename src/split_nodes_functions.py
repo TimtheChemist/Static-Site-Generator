9982a841bbb4ec29ef80356c8a5ded11fd735605
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-01 17:02:53 +0000

    Add incomplete split_nodes_link() function

--- src/split_nodes_functions.py
+++ src/split_nodes_functions.py
@@ -36,11 +36,49 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
     return list_of_nodes
 
 
+def split_nodes_image(old_nodes):
+    pass
+
+
+def split_nodes_link(old_nodes):
+    list_of_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            list_of_nodes.append(old_node)
+
+        else:
+            list_of_urls = extract_markdown_links(old_node.text)
+            matched_linked_text = list_of_urls[0][0]
+            matched_url = list_of_urls[0][1]
+
+
+            list_of_strings = old_node.text.split(f"![{matched_linked_text}]({matched_url})", 1)
+
+
+            string_counter = 1
+            for text_string in list_of_strings:
+
+                if text_string == "":
+                    string_counter += 1
+                    continue
+
+                if string_counter % 2 != 0:
+                    new_node = TextNode(text_string, TextType.TEXT)
+                    list_of_nodes.append(new_node)
+                    string_counter += 1
+
+                else:
+                    new_node = TextNode(text_string, TextType.LINK)
+                    list_of_nodes.append(new_node)
+                    string_counter += 1
+    
+    return list_of_nodes
+
+
 def extract_markdown_images(text):
     matches_alt_text = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
     return matches_alt_text
-
-
 
 
 def extract_markdown_links(text):
