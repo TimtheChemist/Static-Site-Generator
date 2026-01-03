c3f0ec2a689fa62ffe1a236f8d251ea6dff9df27
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-03 11:13:33 +0000

    Add block_to_block_type() and unit tests

--- src/blocks_functions.py
+++ src/blocks_functions.py
@@ -1,4 +1,71 @@
+from enum import Enum
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered_list"
+    ORDERED_LIST = "ordered_list"
+
+
+def block_to_block_type(block):
+
+    if block.startswith("```") and block.endswith("```"):
+        return BlockType.CODE
+
+    elif block.startswith("#"):
+        count = 0
+        for ch in block:
+            if ch == "#":
+                count += 1
+            else:
+                break
+        if len(block) > count:
+            if count < 7 and block[count] == " ":
+                return BlockType.HEADING
+
+    elif block.startswith(">"):
+        lines = block.split("\n")
+        tracker = True
+        for line in lines:
+            if line.startswith(">") == False:
+                tracker = False
+                break
+
+        if tracker == True:
+            return BlockType.QUOTE
+        else:
+            return BlockType.PARAGRAPH
+
+    elif block.startswith("- "):
+        lines = block.split("\n")
+        tracker = True
+        for line in lines:
+            if line.startswith("- ") == False:
+                tracker = False
+                break
+
+        if tracker == True:
+            return BlockType.UNORDERED_LIST
+        else:
+            return BlockType.PARAGRAPH
+
+    elif block.startswith("1. "):
+        lines = block.split("\n")
+        tracker = True
+        for i in range(len(lines)):
+            if lines[i].startswith(f"{i + 1}. ") == False:
+                tracker = False
+                break
+
+        if tracker == True:    
+            return BlockType.ORDERED_LIST
+        else:
+            return BlockType.PARAGRAPH
+
+    else:
+        return BlockType.PARAGRAPH
 
 
 def markdown_to_blocks(markdown):
--- src/test_blocks_functions.py
+++ src/test_blocks_functions.py
@@ -1,5 +1,5 @@
 import unittest
-from blocks_functions import markdown_to_blocks
+from blocks_functions import markdown_to_blocks, block_to_block_type, BlockType
 
 
 class TestBlocksFunctions(unittest.TestCase):
@@ -23,3 +23,40 @@ class TestBlocksFunctions(unittest.TestCase):
                 "- This is a list\n- with items",
             ],
         )
+
+    def test_block_to_block_type_Heading_one(self):
+        block = "# Heading one"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+
+    def test_block_to_block_type_Heading_six(self):
+        block = "###### Heading six"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.HEADING)
+
+
+    def test_block_to_block_type_Code(self):
+        block = "```Code Text Here```"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_block_to_block_type_ordered_list(self):
+        block = "1. one\n2. two\n3. three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_block_to_block_type_ordered_list_wrong_number(self):
+        block = "1. one\n3. three\n2. two"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_block_to_block_type_unordered_list(self):
+        block = "- one\n- two\n- three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_block_to_block_type_unordered_list_interrupted(self):
+        block = "- one\n-two\n- three"
+        block_type = block_to_block_type(block)
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
