a94d7a897c622ea65959c4b901975aea7ac7e3a0
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-10 10:32:46 +0000

    Add copy_static.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from textnode import TextType, TextNode
+from pathlib import Path
+import sys
+sys.path.insert(0, str(Path(__file__).parent.parent))
+from static.copy_static import copy_static
 
 def main():
-    new_object = TextNode("Placeholder text", TextType.BOLD_TEXT)
+    new_object = TextNode("Placeholder text", TextType.BOLD)
     print(new_object)
+    copy_static("/home/timot/workspace/github.com/Static-Site-Generator/static","/home/timot/workspace/github.com/Static-Site-Generator/public")
 
 
 main()
