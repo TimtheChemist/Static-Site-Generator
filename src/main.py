7bdbc8223c41ec3046492cf916498619b35a5dda
TimtheChemist <timothy.kwok.e@gmail.com>
2026-01-11 13:10:37 +0000

    Bug fix to file paths to enable access to site assets

--- src/main.py
+++ src/main.py
@@ -6,12 +6,13 @@ from static.copy_static import copy_static
 from generate_page import generate_page, generate_pages_recursive
 import os
 
-def main():
+dir_path_public = "docs"
 
-    copy_static("static", "docs")
+def main():
+    copy_static("static", dir_path_public)
 
     base_path = sys.argv[1] if len(sys.argv) > 1 else "/"
-    generate_pages_recursive("content", "template.html", "docs", base_path)
+    generate_pages_recursive("content", "template.html", dir_path_public, base_path)
 
 
 
